ac8343ca4bb18bc98712a027f00e1918e34f3b5a
Thomas Lacombe <tomtom637@gmail.com>
2021-11-01 14:55:01 +0100

    added characters colored on input

--- js/e-listeners-handlers.js
+++ js/e-listeners-handlers.js
@@ -1,6 +1,31 @@
 export function handleInput(e) {
   e.preventDefault();
+
+  // Change the background color when an input matches with available letters
+  // TODO take spaces into account
   const input = document.querySelector('input');
+  const allSpans = document.querySelectorAll('.char');
+  
+  // reinitialize the background color to default
+  allSpans.forEach(span => span.style.background = '#ddd');
+
+  const charInInput = input.value.replace(/'/g, "’").toLowerCase().split('');
+  const charInWord = input.dataset.word.replace(/'/g, "’").toLowerCase().split('');
+  const charToColor =  [];
+
+  for(let i = 0; i < charInInput.length; ++i) {
+    if (charInWord.includes(charInInput[i])) {
+      charToColor.push(charInInput[i]);
+    }
+  }
+  allSpans.forEach(span => {
+    if(charToColor.includes(span.innerText.toLowerCase())
+      && span.style.background !== 'rgba(238, 34, 68, 0.3)') {
+      span.style.background = 'rgba(238, 34, 68, 0.3)';
+      charToColor.splice(charToColor.indexOf(span.innerText.toLowerCase()), 1);
+    }
+  });
+
   if(input.value.toLowerCase().trim().replace(/'/g, "’") === (input.dataset.word.toLowerCase().trim().replace(/'/g, "’"))) {
     document.querySelector('.bravo').style.opacity = "1";
   }
